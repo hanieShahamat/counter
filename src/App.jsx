914ed84a9c7ed9c95efeecafd75182b4d219009a
hanie <hanieshahamat@gmail.com>
2024-06-15 10:45:14 +0330

    fix setCount

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,6 @@
 import { useState } from 'react'
-import './App.css'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faCircleInfo } from '@fortawesome/free-solid-svg-icons'
-import { faGear } from '@fortawesome/free-solid-svg-icons'
-import { faArrowsRotate } from '@fortawesome/free-solid-svg-icons'
+import { faCircleInfo,faGear,faArrowsRotate } from '@fortawesome/free-solid-svg-icons'
 
 
 function App() {
@@ -14,7 +11,7 @@ function App() {
       <div className="flex items-center justify-center py-12 my-12">
       <FontAwesomeIcon icon={faCircleInfo} className="ri-information-line text-gray-300 px-7" />
       <FontAwesomeIcon icon={faGear} className="ri-settings-3-line text-gray-300" />
-      <FontAwesomeIcon icon={faArrowsRotate} className="ri-loop-right-line text-gray-300 px-7" onClick={() => setCount((count) => count=0)} />
+      <FontAwesomeIcon icon={faArrowsRotate} className="ri-loop-right-line text-gray-300 px-7" onClick={()=>setCount(0)} />
       </div>
       <h1 className="text-gray-300 flex items-center justify-center text-9xl font-bold">{count}</h1>
       <div className="text-gray-300 flex items-center justify-center py-12 mt-14">
